0498e5586f7d394e1d372ad3aaf8df1a7feb9934
dylan sim <e1527262@u.nus.edu>
2026-08-21 14:52:21 +0800

    Complete Level-2: Added memory and listing features

--- src/main/java/Rem.java
+++ src/main/java/Rem.java
@@ -18,21 +18,32 @@ public class Rem {
         System.out.println("Rem: No more sleeping. Need help?");
         System.out.println(SEPARATOR);
 
+        //Initializes storage and counter for user input
+        String[] added = new String[100];
+        int addCount = 0;
+
         //Scanner to pick up user input and echo accordingly (Level-1)
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("Me: ");
             String command = scanner.nextLine();
+            System.out.println(SEPARATOR);
 
             if (command.equalsIgnoreCase("bye")) {
-                System.out.println(SEPARATOR);
+
                 System.out.println("Rem: [Yawn] Need more sleep. Time for bed...");
                 System.out.println(SEPARATOR);
                 break;
+            } else if (command.equalsIgnoreCase("list")) {
+                for (int i = 0; i < addCount; i++) {
+                    System.out.println("Rem: " + (i + 1) + ". " + added[i]);
+                }
+            } else {
+                added[addCount] = command;
+                addCount++;
+                System.out.println("Rem: added: " + command);
             }
 
-            System.out.println(SEPARATOR);
-            System.out.println("Rem: " + command);
             System.out.println(SEPARATOR);
         }
 
